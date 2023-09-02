373d8e2ea4a95f29a3acd57423ad56ce79cbb66b
RuanUrb <ruaurb@alunos.utfpr.edu.br>
2023-09-02 16:22:36 -0300

    Data sanitization added (JOI + mongoose-sanitize). Helmet setup to prevent XSS.

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user.js')
+const helmet = require('helmet')
+
+const mongoSanitize = require('express-mongo-sanitize')
 
 
 const userRouters = require('./routes/user.js')
@@ -39,10 +42,59 @@ app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(morgan('tiny'))
 app.use(express.static(path.join(__dirname, 'public'))) // links express to public resource folder
+app.use(mongoSanitize())
+app.use(helmet())
+
+// helmet exceptions for content policy
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dlwh01eif/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 // session configuration cookie
 const sessionConfig = {
+	name: 'mc',
 	secret: 'secrettohideyourpowerlevel',
+	//secure: true,        [SHOULD BE ON WHEN IN PRODUCTION] 
 	resave: false,
 	saveUninitialized: true,
 	cookie: {
